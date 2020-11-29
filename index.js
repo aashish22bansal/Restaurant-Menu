96e6a7bcc71da98268cab47d3de7f05530ab8785
Ajith2001 <52373422+Ajith2001@users.noreply.github.com>
2020-11-29 14:22:19 +0530

    aa

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index"); // index refers to index.ejs
+    res.render("index");
+});
+
+app.post("/login", (req, res) => {
+    const { name, password } = req.body;
+
+    if (name === "admin" && password === "admin") {
+        res.render("success", {
+            username: name,
+        });
+    } else {
+        res.render("failure");
+    }
 });
 
 app.listen(3000, () => {
